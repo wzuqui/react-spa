fdb1da7a8052bec05555ea3aa2c7c0c013a5f149
Willian Luis Zuqui <willianluiszuqui@gmail.com>
2022-12-25 23:15:07 +0000

    fix(pagina-inicial): :lipstick: ajustado para telas menos que 720 altura com scrollview

--- src/layouts/Historico.tsx
+++ src/layouts/Historico.tsx
@@ -1,4 +1,4 @@
-import { TileView as TileViewBase } from 'devextreme-react';
+import { ScrollView, TileView as TileViewBase } from 'devextreme-react';
 import { useState } from 'react';
 
 import { IHistorico } from '../entities/historico';
@@ -31,34 +31,38 @@ export function Historico({ acaoAbrir, ...props }: HistoricoProps) {
   }
 
   return (
-    <Container {...props}>
-      {Object.entries(historico).map(([key, value]: [string, IHistorico[]]) => (
-        <Item
-          key={key}
-          hidden={value.length === 0}
-        >
-          <Titulo>{transformarCamelCaseEmTexto(key)}</Titulo>
-          <TileView
-            baseItemHeight={90}
-            baseItemWidth={120}
-            direction="vertical"
-            height={140}
-            itemRender={data => (
-              <HistoricoItem
-                rota={data.rota}
-                acaoFavoritar={acaoFavoritar}
-                acaoDesfavoritar={acaoDesfavoritar}
-                acaoRemover={() => acaoRemover(key, data.rota)}
+    <ScrollView>
+      <Container {...props}>
+        {Object.entries(historico).map(
+          ([key, value]: [string, IHistorico[]]) => (
+            <Item
+              key={key}
+              hidden={value.length === 0}
+            >
+              <Titulo>{transformarCamelCaseEmTexto(key)}</Titulo>
+              <TileView
+                baseItemHeight={90}
+                baseItemWidth={120}
+                direction="vertical"
+                height={140}
+                itemRender={data => (
+                  <HistoricoItem
+                    rota={data.rota}
+                    acaoFavoritar={acaoFavoritar}
+                    acaoDesfavoritar={acaoDesfavoritar}
+                    acaoRemover={() => acaoRemover(key, data.rota)}
+                  />
+                )}
+                items={value}
+                onItemClick={e => acaoAbrir(e.itemData!.rota)}
+                showScrollbar="never"
+                width="100%"
               />
-            )}
-            items={value}
-            onItemClick={e => acaoAbrir(e.itemData!.rota)}
-            showScrollbar="never"
-            width="100%"
-          />
-        </Item>
-      ))}
-    </Container>
+            </Item>
+          )
+        )}
+      </Container>
+    </ScrollView>
   );
 }
 
